a5576898b3f12d2060faff265b6b082ab3f142ef
te-abhishek-kumar <abhishek.k01@testyantra.in>
2022-05-11 12:42:45 +0530

    updated subject repository

--- src/main/java/com/task/student/repository/SubjectRepository.java
+++ src/main/java/com/task/student/repository/SubjectRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.task.student.entity.Subject;
 
+@Repository
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
 
 }
